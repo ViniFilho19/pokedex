1cb96e56aa45b0fe7c239ea44bb8d9e03ebd036a
vinicius <mvinicraft@gmail.com>
2024-07-21 23:08:57 -0300

    pokedex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue';
+
+import './style/main.scss'
+
 import App from './App.vue';
 
 Vue.config.productionTip = false;
+vue.use(InfiniteLoading, {});
 
 new Vue({
-  render: (h) => h(App),
-}).$mount('#app');
+	render: h => h(App),
+}).$mount('#root');
